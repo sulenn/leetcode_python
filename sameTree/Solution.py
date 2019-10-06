c46367b5023ab1911af74a20b0faebd2b3c609c6
sulenn <273409891@qq.com>
2019-10-06 22:32:03 +0800

    init project

---
+++ sameTree/Solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if p == None and q == None:
+            return True
+        if p != None and q != None and p.val == q.val:
+            left = self.isSameTree(p.left, q.left)
+            right = self.isSameTree(p.right, q.right)
+            return left and right
+        return False
