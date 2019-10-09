3295b8599a8233bf11a659d7031ca0a464c591e4
sulenn <273409891@qq.com>
2019-10-09 09:05:00 +0800

    completely solve isSymmetric algrithm

--- isSymmetric/Solution.py
+++ isSymmetric/Solution.py
@@ -2,32 +2,6 @@
 from TreeNode import TreeNode
 class Solution(object):
     # iterative method
-    # def isSymmetric(self, root):
-    #     """
-    #     :type root: TreeNode
-    #     :rtype: bool
-    #     """
-    #     if not root:
-    #         return True
-    #     level = 2
-    #     curLevelList = [root.left, root.right]
-    #     while len(curLevelList) != 0:
-    #         for j in range(len(curLevelList)/2):
-    #             if not curLevelList[j] and not curLevelList[len(curLevelList)-j-1]:
-    #                 continue
-    #             if not curLevelList[j] or not curLevelList[len(curLevelList)-j-1]:
-    #                 return False
-    #             if curLevelList[j].val != curLevelList[len(curLevelList)-j-1].val:
-    #                 return False
-    #         nextLevelList = []
-    #         for i in range(len(curLevelList)):
-    #             if curLevelList[i] is not None:
-    #                 nextLevelList += [curLevelList[i].left]
-    #                 nextLevelList += [curLevelList[i].right]
-    #         curLevelList = nextLevelList
-    #         level += 1
-    #     return True
-
     def isSymmetric(self, root):
         """
         :type root: TreeNode
@@ -35,15 +9,43 @@ class Solution(object):
         """
         if not root:
             return True
-        return self.recursive(root.left,root.right)
+        level = 2
+        curLevelList = [root.left, root.right]
+        while len(curLevelList) != 0:
+            for j in range(len(curLevelList)/2):
+                if not curLevelList[j] and not curLevelList[len(curLevelList)-j-1]:
+                    continue
+                if not curLevelList[j] or not curLevelList[len(curLevelList)-j-1]:
+                    return False
+                if curLevelList[j].val != curLevelList[len(curLevelList)-j-1].val:
+                    return False
+            nextLevelList = []
+            for i in range(len(curLevelList)):
+                if curLevelList[i] is not None:
+                    nextLevelList += [curLevelList[i].left]
+                    nextLevelList += [curLevelList[i].right]
+            curLevelList = nextLevelList
+            level += 1
+        return True
 
-    def recursive(self, leftNode, rightNode):
-        if not leftNode and not rightNode:
-            return True
-        if not leftNode or not rightNode:
-            return False
-        if self.recursive(leftNode.left, rightNode.right) and self.recursive(leftNode.right, rightNode.left) and leftNode.val == rightNode.val:
-            return True
+    # recursive method
+    # def isSymmetric(self, root):
+    #     """
+    #     :type root: TreeNode
+    #     :rtype: bool
+    #     """
+    #     if not root:
+    #         return True
+    #     return self.recursive(root.left,root.right)
+    #
+    # def recursive(self, leftNode, rightNode):
+    #     if not leftNode and not rightNode:
+    #         return True
+    #     if not leftNode or not rightNode:
+    #         return False
+    #     if leftNode.val != rightNode.val:
+    #         return False
+    #     return self.recursive(leftNode.left, rightNode.right) and self.recursive(leftNode.right, rightNode.left)
 
 if __name__ == '__main__':
     n = TreeNode(1)
