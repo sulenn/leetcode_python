29d5cb8c6f35c51d3f6686c1333a2485bf25735f
sulenn <273409891@qq.com>
2019-10-13 14:56:01 +0800

    completely solve employeeImportance algorithm with recursive and none-recursive method

--- employeeImportance/Solution.py
+++ employeeImportance/Solution.py
@@ -12,49 +12,40 @@ class Employee(object):
         self.subordinates = subordinates
 """
 class Solution(object):
+    # recursive way
     def getImportance(self, employees, id):
         """
-        :type employees: Employee
+        :type employees: Employee list
         :type id: int
         :rtype: int
         """
-        root = self.generateTree(employees)
-        idNode = self.findNode(root, id)
-        if idNode:
-            return self.curCalculateImportance(idNode)
-        return 0
+        nodeDic = {node.id: node for node in employees}
+        def f(i):
+            e = nodeDic[i]
+            return e.importance + sum(map(f, e.subordinates))
+        return f(id)
 
-    def generateTree(self, employees):
-        if not employees:
-            return None
-        nodeList = list()
-        for i in employees:
-            nodeList.append(Employee(i[0], i[1], i[2]))
-        for node in nodeList:
-            subNodeList = []
-            for j in node.subordinates:
-                subNodeList.append(nodeList[j-1])
-            node.subordinates = subNodeList
-        return nodeList[0]
 
-    def findNode(self, employees, id):
-        if not employees:
-            return None
-        if employees.id == id:
-            return employees
-        for node in employees.subordinates:
-            if self.findNode(node):
-                return node
-        return None
+    # none recursive way
+    # def getImportance(self, employees, id):
+    #     """
+    #     :type employees: Employee list
+    #     :type id: int
+    #     :rtype: int
+    #     """
+    #     nodeDic = {node.id: node for node in employees}
+    #     realEmployee = nodeDic[id]
+    #     if not realEmployee:
+    #         return 0
+    #     nodeList = [realEmployee]
+    #     importance = 0
+    #     while nodeList:
+    #         node = nodeList.pop()
+    #         importance += node.importance
+    #         for i in node.subordinates:
+    #             nodeList.append(nodeDic[i])
+    #     return importance
 
-    def curCalculateImportance(self, idNode):
-        if not idNode:
-            return 0
-        importance = idNode.importance
-        for node in idNode.subordinates:
-            importance += self.curCalculateImportance(node)
-        return importance
 
-if __name__ == '__main__':
-    s = Solution()
-    print s.getImportance([[1, 5, [2, 3]], [2, 3, []], [3, 3, []]], 1)
+
+
