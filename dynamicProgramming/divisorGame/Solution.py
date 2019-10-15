f22461c30694f93278a297e2b0b24876e92fba13
sulenn <273409891@qq.com>
2019-10-16 00:40:55 +0800

    add and solve divisorGame algorithm with math's method

---
+++ dynamicProgramming/divisorGame/Solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def divisorGame(self, N):
+        """
+        :type N: int
+        :rtype: bool
+        """
+        if N % 2 == 0:
+            return True
+        else:
+            return False
+
+if __name__ == '__main__':
+    print 3//2
+    print range(1,1)
